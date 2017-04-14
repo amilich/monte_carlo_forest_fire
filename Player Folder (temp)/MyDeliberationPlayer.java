69df3519ddb44f3588aa7c5869d1f5c67c5cd3cd
Andrew <milichab@gmail.com>
2017-04-14 15:26:28 -0700

    minimax

---
+++ Player Folder (temp)/MyDeliberationPlayer.java
@@ -0,0 +1,119 @@
+import java.util.ArrayList;
+import java.util.List;
+
+import org.ggp.base.player.gamer.exception.GamePreviewException;
+import org.ggp.base.player.gamer.statemachine.StateMachineGamer;
+import org.ggp.base.util.game.Game;
+import org.ggp.base.util.statemachine.MachineState;
+import org.ggp.base.util.statemachine.Move;
+import org.ggp.base.util.statemachine.Role;
+import org.ggp.base.util.statemachine.StateMachine;
+import org.ggp.base.util.statemachine.cache.CachedStateMachine;
+import org.ggp.base.util.statemachine.exceptions.GoalDefinitionException;
+import org.ggp.base.util.statemachine.exceptions.MoveDefinitionException;
+import org.ggp.base.util.statemachine.exceptions.TransitionDefinitionException;
+import org.ggp.base.util.statemachine.implementation.prover.ProverStateMachine;
+
+public class MyDeliberationPlayer extends StateMachineGamer {
+	@Override
+	public StateMachine getInitialStateMachine() {
+		return new CachedStateMachine(new ProverStateMachine());
+	}
+
+	@Override
+	public void stateMachineMetaGame(long timeout)
+			throws TransitionDefinitionException, MoveDefinitionException, GoalDefinitionException {
+		// TODO Auto-generated method stub
+	}
+
+	/**
+	 * Function: stateMachineSelectMove
+	 * ---------------------------------
+	 * Recurses down game tree and selects move that leads to highest possible score outcome.
+	 */
+	@Override
+	public Move stateMachineSelectMove(long timeout)
+			throws TransitionDefinitionException, MoveDefinitionException, GoalDefinitionException {
+		StateMachine machine = getStateMachine();
+		MachineState currState = getCurrentState();
+		Move action = null;
+		try {
+			action = bestmove(getRole(), currState, machine);
+		} catch(Exception e) {
+			System.out.println("Error occurred while finding action: " + e);
+			return machine.getLegalMoves(currState, getRole()).get(0);
+		}
+		return action;
+	}
+
+	/**
+	 * Function: bestmove
+	 * -------------------
+	 * Iterates through moves at each step and calls function to determine max
+	 * score achievable from move.
+	 */
+	private Move bestmove(Role role, MachineState state, StateMachine machine) throws MoveDefinitionException,
+		GoalDefinitionException, TransitionDefinitionException {
+
+		List<Move> actions = machine.getLegalMoves(state, role);
+		double score = 0.0;
+		Move finalMove = actions.get(0);
+		for (int ii = 0; ii < actions.size(); ii ++) {
+			List<Move> tempMoves = new ArrayList<Move>();
+			tempMoves.add(actions.get(ii));
+			double result = maxscore(role, machine.getNextState(state, tempMoves), machine);
+			if (result == 100) return actions.get(ii);
+			if (result > score) {
+				score = result;
+				finalMove = actions.get(ii);
+			}
+		}
+		return finalMove;
+	}
+
+	/**
+	 * Function: maxscore
+	 * -------------------
+	 * Recursively determine max score that can be achieved from choosing a given move
+	 * in a given game state.
+	 */
+	private double maxscore(Role role, MachineState currState, StateMachine machine) throws MoveDefinitionException,
+		GoalDefinitionException, TransitionDefinitionException {
+		if (machine.isTerminal(currState)) {
+			return machine.getGoal(currState, role); // TODO correct value
+		}
+		List<Move> actions = machine.getLegalMoves(currState, role);
+		double score = 0.0;
+		for (Move action : actions) {
+			List<Move> tempMoves = new ArrayList<Move>(); // TODO add other roles
+			tempMoves.add(action);
+			double result = maxscore(role, machine.getNextState(currState, tempMoves), machine);
+			if (result == 100) return result;
+			if (result > score) score = result; // TODO short circuit
+		}
+		return score;
+	}
+
+	@Override
+	public void stateMachineStop() {
+		// TODO Auto-generated method stub
+
+	}
+
+	@Override
+	public void stateMachineAbort() {
+		// TODO Auto-generated method stub
+
+	}
+
+	@Override
+	public void preview(Game g, long timeout) throws GamePreviewException {
+		// TODO Auto-generated method stub
+
+	}
+
+	@Override
+	public String getName() {
+		return "CompulsivePlayer";
+	}
+}
---
+++ Player Folder (temp)/MyMinimaxPlayer.java
@@ -0,0 +1,166 @@
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.List;
+
+import org.ggp.base.player.gamer.exception.GamePreviewException;
+import org.ggp.base.player.gamer.statemachine.StateMachineGamer;
+import org.ggp.base.util.game.Game;
+import org.ggp.base.util.statemachine.MachineState;
+import org.ggp.base.util.statemachine.Move;
+import org.ggp.base.util.statemachine.Role;
+import org.ggp.base.util.statemachine.StateMachine;
+import org.ggp.base.util.statemachine.cache.CachedStateMachine;
+import org.ggp.base.util.statemachine.exceptions.GoalDefinitionException;
+import org.ggp.base.util.statemachine.exceptions.MoveDefinitionException;
+import org.ggp.base.util.statemachine.exceptions.TransitionDefinitionException;
+import org.ggp.base.util.statemachine.implementation.prover.ProverStateMachine;
+
+public class MyMinimaxPlayer extends StateMachineGamer {
+	@Override
+	public StateMachine getInitialStateMachine() {
+		return new CachedStateMachine(new ProverStateMachine());
+	}
+
+	@Override
+	public void stateMachineMetaGame(long timeout)
+			throws TransitionDefinitionException, MoveDefinitionException, GoalDefinitionException {
+		// TODO Auto-generated method stub
+	}
+
+
+	/**
+	 * Controls printing of debug statements.
+	 */
+	final boolean DEBUG_EN = false;
+	final double MAX_SCORE = 100;
+	final double MIN_SCORE = 0;
+
+	/**
+	 * Function: stateMachineSelectMove
+	 * ----------------------------------
+	 * Returns move for the player at a given stage.
+	 */
+	@Override
+	public Move stateMachineSelectMove(long timeout)
+			throws TransitionDefinitionException, MoveDefinitionException, GoalDefinitionException {
+		if (DEBUG_EN) System.out.println("Selecting move for " + getRole());
+		MachineState currState = getCurrentState();
+		Move action = bestmove(getRole(), currState);
+//		try {
+//			action = bestmove(getRole(), currState, machine);
+//		} catch(Exception e) {
+//			System.out.println("Error occurred while finding action: " + e);
+//			return machine.getLegalMoves(currState, getRole()).get(0);
+//		}
+		if (DEBUG_EN) System.out.println("Selected action (role = " + getRole() + ") = " + action);
+		return action;
+	}
+
+	/**
+	 * Function: bestmove
+	 * -------------------
+	 * Return best possible move using minimax strategy.
+	 */
+	private Move bestmove(Role role, MachineState state) throws MoveDefinitionException,
+	GoalDefinitionException, TransitionDefinitionException {
+		List<Move> actions = new ArrayList<Move>();
+		actions.addAll(getStateMachine().getLegalMoves(state, role));
+		Collections.shuffle(actions);
+		double score = MIN_SCORE;
+		Move finalMove = null;
+		System.out.println("actions: " + actions);
+		for (int ii = 0; ii < actions.size(); ii ++) {
+			double result = minscore(role, actions.get(ii), state);
+			if (result > score) {
+				score = result;
+				finalMove = actions.get(ii);
+			}
+			if (score == MAX_SCORE) return actions.get(ii);
+		}
+		if (finalMove == null) {
+			System.out.println("NULL");
+			return actions.get(0);
+		}
+		return finalMove;
+	}
+
+	/**
+	 * Function: opponent
+	 * -------------------
+	 * Find opponent player. Unused if getLegalJointMoves is called.
+	 */
+	@SuppressWarnings("unused")
+	private Role opponent(Role role) {
+		List<Role> roles = getStateMachine().getRoles();
+		for (Role r : roles) {
+			if (!r.equals(role)) return r;
+		}
+		return role; // TODO error
+	}
+
+	/**
+	 * Function: minscore
+	 * -------------------
+	 * Recursively determine minimum score that can be achieved from choosing a given move
+	 * in a given game state (assuming rational opponent).
+	 */
+	private double minscore(Role role, Move move, MachineState state) throws MoveDefinitionException,
+	TransitionDefinitionException, GoalDefinitionException {
+		List<List<Move>> jointActions = getStateMachine().getLegalJointMoves(state, role, move);
+		double score = MAX_SCORE;
+		for (List<Move> jointAction : jointActions) { // Opponent's move
+			MachineState nextState = getStateMachine().getNextState(state, jointAction);
+			double result = maxscore(role, nextState);
+			if (result == MIN_SCORE) return result;
+			if (result < score) {
+				score = result;
+				if (DEBUG_EN) System.out.println("Low score of " + score + " if we play " + jointAction);
+			}
+		}
+		return score;
+	}
+
+	/**
+	 * Function: maxscore
+	 * -------------------
+	 * Determine max score that can be achieved from choosing a given move
+	 * in a given game state by maximizing minimum score (minimax).
+	 */
+	private double maxscore(Role role, MachineState currState) throws MoveDefinitionException,
+	GoalDefinitionException, TransitionDefinitionException {
+		if (getStateMachine().isTerminal(currState)) {
+			return getStateMachine().getGoal(currState, role);
+		}
+		List<Move> actions = getStateMachine().getLegalMoves(currState, role);
+		double score = MIN_SCORE;
+		for (Move action : actions) {
+			double result = minscore(role, action, currState);
+			if (result == MAX_SCORE) return result;
+			if (result > score) score = result;
+		}
+		return score;
+	}
+
+	@Override
+	public void stateMachineStop() {
+		// TODO Auto-generated method stub
+
+	}
+
+	@Override
+	public void stateMachineAbort() {
+		// TODO Auto-generated method stub
+
+	}
+
+	@Override
+	public void preview(Game g, long timeout) throws GamePreviewException {
+		// TODO Auto-generated method stub
+
+	}
+
+	@Override
+	public String getName() {
+		return "MinimaxPlayer";
+	}
+}
