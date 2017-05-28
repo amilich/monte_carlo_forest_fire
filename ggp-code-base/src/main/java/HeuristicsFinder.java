0d9b67d9b406c39314ced09c3cba0e51373cf941
sigberto <salarcon@stanford.edu>
2017-05-27 17:04:40 -0700

    Started heuristics finder

---
+++ ggp-code-base/src/main/java/HeuristicsFinder.java
@@ -0,0 +1,18 @@
+public class HeuristicsFinder {
+	// using logic from Sancho's blog post: http://sanchoggp.blogspot.com/2014/08/goal-latches.html
+//	public static List<Proposition> findLatches(List<Component> allComponents){
+//		List<Component> latches = new ArrayList<Component>();
+//		for (Component c : allComponents){
+//			if (c instanceof And){
+//				for (Component output : c.output_arr){
+//					for (Component input : c.input_arr){
+//						if (input == output){
+//
+//						}
+//					}
+//				}
+//			}
+//		}
+//		return latches;
+//	}
+}
