e5afc60afc2e29ea57cab16ad3b35af276d85655
Andrew <milichab@gmail.com>
2017-05-29 22:39:56 -0700

    start

--- ggp-code-base/src/main/java/org/ggp/base/util/statemachine/implementation/propnet/ExpPropNet.java
+++ ggp-code-base/src/main/java/org/ggp/base/util/statemachine/implementation/propnet/ExpPropNet.java
@@ -11,8 +11,14 @@ import java.util.Queue;
 import java.util.Set;
 
 import org.ggp.base.util.gdl.grammar.Gdl;
+import org.ggp.base.util.gdl.grammar.GdlDistinct;
+import org.ggp.base.util.gdl.grammar.GdlFunction;
+import org.ggp.base.util.gdl.grammar.GdlLiteral;
+import org.ggp.base.util.gdl.grammar.GdlPool;
 import org.ggp.base.util.gdl.grammar.GdlRelation;
+import org.ggp.base.util.gdl.grammar.GdlRule;
 import org.ggp.base.util.gdl.grammar.GdlSentence;
+import org.ggp.base.util.gdl.grammar.GdlTerm;
 import org.ggp.base.util.propnet.architecture.Component;
 import org.ggp.base.util.propnet.architecture.PropNet;
 import org.ggp.base.util.propnet.architecture.components.And;
@@ -59,9 +65,57 @@ public class ExpPropNet extends StateMachine {
 	public int allLongs[] = null;
 	public Component[] allCompArr = null;
 
+	private void sanitizeDistinctHelper(Gdl gdl, List<Gdl> in, List<Gdl> out) {
+	    if (!(gdl instanceof GdlRule)) {
+	        out.add(gdl);
+	        return;
+	    }
+	    GdlRule rule = (GdlRule) gdl;
+	    for (GdlLiteral lit : rule.getBody()) {
+	        if (lit instanceof GdlDistinct) {
+	            GdlDistinct d = (GdlDistinct) lit;
+	            GdlTerm a = d.getArg1();
+	            GdlTerm b = d.getArg2();
+	            if (!(a instanceof GdlFunction) && !(b instanceof GdlFunction)) continue;
+	            if (!(a instanceof GdlFunction && b instanceof GdlFunction)) return;
+	            GdlSentence af = ((GdlFunction) a).toSentence();
+	            GdlSentence bf = ((GdlFunction) b).toSentence();
+	            if (!af.getName().equals(bf.getName())) return;
+	            if (af.arity() != bf.arity()) return;
+	            for (int i = 0; i < af.arity(); i++) {
+	                List<GdlLiteral> ruleBody = new ArrayList<>();
+	                for (GdlLiteral newLit : rule.getBody()) {
+	                    if (newLit != lit) ruleBody.add(newLit);
+	                    else ruleBody.add(GdlPool.getDistinct(af.get(i), bf.get(i)));
+	                }
+	                GdlRule newRule = GdlPool.getRule(rule.getHead(), ruleBody);
+	                // Log.println("new rule: " + newRule);
+	                in.add(newRule);
+	            }
+	            return;
+	        }
+	    }
+	    for (GdlLiteral lit : rule.getBody()) {
+	        if (lit instanceof GdlDistinct) {
+	            System.out.println("distinct rule added: " + rule);
+	            break;
+	        }
+	    }
+	    out.add(rule);
+	}
+
+	private List<Gdl> sanitizeDistinct(List<Gdl> description) {
+	    List<Gdl> out = new ArrayList<>();
+	    for (int i = 0; i < description.size(); i++) {
+	        sanitizeDistinctHelper(description.get(i), description, out);
+	    }
+	    return out;
+	}
+
 	@Override
 	public void initialize(List<Gdl> description, Role r) {
 		System.out.println("[PropNet] Initializing for role " + r);
+		description = sanitizeDistinct(description);
 		try {
 			propNet = OptimizingPropNetFactory.create(description);
 			roles = propNet.getRoles().toArray(new Role[propNet.getRoles().size()]);
